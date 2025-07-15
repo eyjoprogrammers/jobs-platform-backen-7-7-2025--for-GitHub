ed33b0e0e0e2847a49cbdffba7312a15f6cabda6
Ey Jo <eyjoprogrammers@gmail.com>
2025-07-15 12:20:16 +0300

    modify

--- config/cors.php
+++ config/cors.php
@@ -1,22 +1,9 @@
 <?php
 return [
-
-    'paths' => ['api/*', '*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://127.0.0.1:3001'],
-    'allowed_headers' => ['*'],
+    'paths' => ['api/*'],
+    'allowed_origins' => ['*'],             // لاحقاً يمكن تحديد النطاق الدقيق
     'allowed_methods' => ['*'],
-
-    'allowed_origins_patterns' => [],
-
-
-    'exposed_headers' => [],
-
+    'allowed_headers' => ['*'],
     'max_age' => 0,
-
     'supports_credentials' => false,
-
-  
-    
-
-];
-
+  ];
